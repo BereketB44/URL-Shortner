ad15ccd9dca9f69b8e8ba07511cecb621f7fde0c
Bereket-Beshane <bereketbeshane44@gmail.com>
2025-07-19 20:14:35 -0500

    initial commit

---
+++ src/main/java/com/porchpick/app/PorchPickApplication.java
@@ -0,0 +1,13 @@
+package com.porchpick.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorchPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorchPickApplication.class, args);
+    }
+
+}
---
+++ steps.md
@@ -0,0 +1,7 @@
+## Steps
+
+1. Create a new project
+2. COnfgiure database IN APP.PROPERTIES
+3. Configure actuator
+4. show actuator/health endpoint
+5. deploy early.
